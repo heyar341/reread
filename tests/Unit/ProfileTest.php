702977e6599c46f941e66dfe0f726397e000e9a0
heyar341 <kityonya@gmail.com>
2020-03-09 17:00:25 +0900

    Profileテスト用のファイル作成

---
+++ tests/Unit/ProfileTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProfileTest extends TestCase
+{
+    /** @test */
+    use RefreshDatabase;
+
+}
