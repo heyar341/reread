1e088513a919e94011485175f6aacb72c3f0cc71
heyar341 <kityonya@gmail.com>
2020-03-09 21:33:12 +0900

    Profileのupdate機能テスト追加

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Profile::class, function (Faker $faker) {
     return [
-        //
+        'intro_self' => $faker->sentence,
+        'prof_url' => $faker->url,
+        'prof_image' => $faker->imageUrl(),
     ];
 });
