55f8df88eb1334f788056b65220238366754f83a
heyar341 <kityonya@gmail.com>
2020-03-12 17:44:28 +0900

    Profileのテストを修正

--- tests/Feature/ProfileEditTest.php
+++ tests/Feature/ProfileEditTest.php
@@ -10,18 +10,30 @@ use Tests\TestCase;
 
 class ProfileEditTest extends TestCase
 {
-    /** @test Profileのedit機能テスト*/
     use RefreshDatabase;
+
+    /** @test Profileのedit機能テスト*/
     public function a_profile_can_be_updated()
     {
         $this->withoutExceptionHandling();
         $user = factory(User::class)->create();
-
-        $response = $this->actingAs($user)->patch('/profile/' . $user->id,[
-//          ダミーデータで書き換え
-            factory(Profile::class)->make(),
-        ]);
+        //UserがProfile編集ページにアクセスする
+        $this->actingAs($user)->get('/profile/'.$user->id.'/edit')->assertOk();
+        //UserがProfile内容を変更する
+        $response = $this->actingAs($user)->patch('/profile/' . $user->id,$this->requestArray());
         $response->assertRedirect("/profile/{$user->id}");
 
+    }
+
+    //Profile更新時のダミーデータ
+    private function requestArray(): array
+    {
+        $data = factory(Profile::class)->make()->toArray();
+        $array = [
+            'intro_self' => $data['intro_self'],
+            'prof_url' => $data['prof_url'],
+            'prof_image' => $data['prof_image'],
+        ];
+        return $array;
     }
 }
