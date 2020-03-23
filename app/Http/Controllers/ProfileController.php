96745b6f7b1cef2c65515b2b646236bfbbb1787d
heyar341 <kityonya@gmail.com>
2020-03-23 20:57:19 +0900

    update時の処理を中心に追加。本番環境で変更する必要あり。

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,14 +7,14 @@ use App\Http\Requests\ProfileRequest;
 use App\Profile;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
     public function index(User $user)
     {
-
-        return view('profiles.index',compact(['user',]));
-
+        return view('profiles.index',compact('user'));
     }
 
     public function edit(User $user)
@@ -29,12 +29,33 @@ class ProfileController extends Controller
     {
         $profile = auth()->user()->profile;
 
-        $profile->intro_self = $request->input('intro_self');
-        $profile->prof_url = $request->input('prof_url');
-        $profile->prof_image = $request->input('prof_image');
+        if(empty($request->input('intro_self'))){
+            $profile->intro_self = 'Not Edited';
+        }
+        else {
+            $profile->intro_self = $request->input('intro_self');
+        }
+        if(empty($request->input('prof_url'))){
+            $profile->prof_url = 'Not Edited';
+        }
+        else {
+            $profile->prof_url = $request->input('prof_url');
+        }
 
-        auth()->user()->profile->save();
+        if($request->prof_image){
+            //画像ファイルを変数に取り込む
+            $imagefile = $request->file('prof_image');
+            //画像の保存先パスを取得
+            $storagePath=$imagefile->store('uploads/profile_image','s3');
+            $image = Image::make($imagefile)->fit(300,300);
+            Storage::disk('s3')->put($storagePath, (string) $image->encode(),'public');
 
+
+            //本番環境ではs3のバケットに合わせて変える
+            $profile->prof_image = "http://localhost:60007/test/".$storagePath;
+        }
+
+        $profile->save();
         return redirect("/profile/{$user->id}");
     }
 }
