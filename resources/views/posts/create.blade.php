57c4d8c92e5ca443d1f5d071ad61d1ea242ac136
heyar341 <kityonya@gmail.com>
2020-03-25 23:55:57 +0900

    書籍情報も含めてPOSTでデータを送信するように変更

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,12 +4,24 @@
     <div class="container">
         <div class="row">
             {{------本の情報--}}
-            <div class="book_info mx-auto d-flex mb-5" style="max-width: 640px">
-                <div class="col-4">
-                    <img src="#book_image_path">
-                </div>
-                <div class="col-8">
-                    <p>Book_description</p>
+            <div class="col-lg-8 mx-auto">
+                <div class="card mb-3" style="max-width: 800px; min-height:150px">
+                    <div class="row">
+                        {{--書籍情報の左側--}}
+                        <div class="col-sm-3 mt-2">
+                            <img src="{{ $book['thumbnail'] }}"
+                                 class="ml-2 mb-1 d-block"
+                                 style="display: inline-block;max-height: 144px">
+                        </div>
+                        {{--書籍情報の右側--}}
+                        <div class="col-sm-9 mt-3">
+                            <h4>{{ $book['title'] }}</h4>
+                            <small>{{ $book['authors'] }}、</small>
+                            <small>出版年：{{ $book['publishedDate'] }}、</small>
+                            <small>{{ $book['pageCount'] }}ページ</small>
+                            <p class="mt-2 mr-2">{{ mb_substr($book['description'],0,100) }}･･･</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -17,6 +29,16 @@
             <div class="col-12">
                 <form action="/post" class="mx-auto" style="max-width: 640px;" method="post">
                     @csrf
+                    {{--書籍の情報--}}
+                    <input type="hidden" name="bookCode" value="{{ $book['bookCode'] }}">
+                    <input type="hidden" name="thumbnail" value="{{ $book['thumbnail'] }}">
+                    <input type="hidden" name="title" value="{{ $book['title'] }}">
+                    <input type="hidden" name="infoLink" value="{{ $book['infoLink'] }}">
+                    <input type="hidden" name="authors" value="{{ $book['authors'] }}">
+                    <input type="hidden" name="publishedDate" value="{{ $book['publishedDate'] }}">
+                    <input type="hidden" name="pageCount" value="{{ $book['pageCount'] }}">
+                    <input type="hidden" name="description" value="{{ $book['description'] }}">
+                    {{--投稿編集--}}
                     <div class="post-comment mb-4">
                         <h5>投稿者コメント：</h5>
                         <textarea style="width: 100%" name="thumbnail_comment" placeholder="投稿一覧ページに表示されます。"></textarea>
