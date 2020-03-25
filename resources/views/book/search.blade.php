f202d6e50e5e28f5b5ea341dbe597d8eca999b8d
heyar341 <kityonya@gmail.com>
2020-03-25 23:52:04 +0900

    投稿の順序の関係から、投稿するボタンのルートを書籍検索ページに変更。

--- resources/views/book/search.blade.php
+++ resources/views/book/search.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="book-search mx-auto">
+                <h2 class="mt-5 mb-3">要約を投稿したい書籍の題名をご入力ください</h2>
                 <form action="search" method="post">
                     @csrf
                     <input class="py-1" type="text" name="book" placeholder="書籍名を入力" style="width: 400px">
