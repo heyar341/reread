81a2fbdbda944b93bcc894f2f9266b541810a3d7
heyar341 <kityonya@gmail.com>
2020-03-25 23:42:48 +0900

    Bookモデルを作成。postとのリレーション記述。

---
+++ app/Book.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $guarded=[];
+
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
+}
