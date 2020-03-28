9d81aab64c260765778fd29c5860a04a076bfe71
heyar341 <kityonya@gmail.com>
2020-03-28 22:46:43 +0900

    HomeController作成。

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
+
     public function index()
     {
-        return view('home');
+        $posts = Post::latest()->paginate(6);
+            return view('home', compact('posts'));
     }
 }
