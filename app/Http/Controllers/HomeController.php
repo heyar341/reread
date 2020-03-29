e2ed5f5e01cdb870ee7303acf0fcc1197db61235
heyar341 <kityonya@gmail.com>
2020-03-29 16:04:36 +0900

    クエリー発行を少なくするように変更。viewでcount関数を使ってお気に入り数をカウントすることで、クエリー発行を抑えられる。

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,15 +8,11 @@ use Illuminate\Pagination\Paginator;
 
 class HomeController extends Controller
 {
-
-//    public function __construct()
-//    {
-//        $this->middleware('auth');
-//    }
-
     public function index()
     {
-        $posts = Post::latest()->paginate(6);
-            return view('home', compact('posts'));
+        $posts = Post::with(['book','isLiked','user'=>
+            function($query){$query->with(['profile'=>
+                function($query){$query->with('followers');}]);}])->where('post_state',1)->latest()->paginate(6);
+        return view('home', compact('posts'));
     }
 }
