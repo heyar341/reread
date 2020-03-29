7204bc20bd8e6810ab8d1507f81f02e4e1a91593
heyar341 <kityonya@gmail.com>
2020-03-29 22:42:42 +0900

    フォロー、フォロワー、投稿一覧表示機能作成。

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MypageController extends Controller
 {
@@ -26,30 +27,88 @@ class MypageController extends Controller
      */
     public function index(User $user,$post_state){
         //投稿したユーザーのみ閲覧可能
-        if(auth()->user()->id == $user->id) {
-            if ($post_state == 1) {
-                $posts = $user->posts()->where('post_state', 1)->get();
-                return view('mypage.postshow', compact('posts','user'));
-
-            }
-            if ($post_state == 2) {
-                $posts = $user->posts()->where('post_state', 2)->get();
-                return view('mypage.postshow', compact('posts','user'));
-
-            }
-            if ($post_state == 3) {
-                $posts = $user->posts()->where('post_state', 3)->get();
-                return view('mypage.postshow', compact('posts','user'));
-
-            }
-            else{
-                return redirect('/')->with('error','URLが無効です。');
-            }
+        if(!Auth::check()){
+            return redirect('/')->with('error','アクセス権限がありません');
         }
 
-        //投稿したユーザー以外は、はじかれる
+
+        if(auth()->user()->id == $user->id) {
+            //投稿状態に応じて、投稿データ取得
+            $posts = $user->posts()->with(['book','is_liked','user.profile.followers'])->where('post_state', $post_state)->latest()->paginate(6);
+
+            //投稿状態に応じて、表示内容変更
+            if ($post_state == 1) {
+                $title = "公開済みの投稿";
+            }
+            if ($post_state == 2) {
+                $title = "非公開の投稿";
+            }
+            if ($post_state == 3) {
+                $title = "下書きの投稿";
+            }
+            return view('mypage.postshow', compact('posts', 'user', 'title'));
+        }
+
+        //URLで４とか入ってたら無効
         else{
-            return redirect('/')->with('error','アクセス権限がありません');
+                return redirect('/')->with('error','URLが無効です。');
+            }
+    }
+
+    public function showall(User $user)
+    {
+        //投稿したユーザーのみ閲覧可能
+        if (!Auth::check()) {
+            return redirect('/')->with('error', 'アクセス権限がありません');
+        }
+        if(auth()->user()->id == $user->id) {
+            //投稿状態に応じて、投稿データ取得
+            $posts = $user->posts()->with(['book', 'is_liked', 'user.profile.followers'])->latest()->paginate(6);
+
+            $title = "全ての投稿一覧";
+            return view('mypage.postshow', compact('posts', 'user', 'title'));
+        }
+        else{
+            return redirect('/')->with('error','他のユーザーのページです。');
+
+        }
+    }
+
+    public function follow(User $user)
+    {
+        //ログインユーザーのみ閲覧可能
+        if (!Auth::check()) {
+            return redirect('/')->with('error', 'アクセス権限がありません');
+        }
+        if(auth()->user()->id == $user->id) {
+            //投稿状態に応じて、投稿データ取得
+            $follows = $user->following()->with('user')->get();
+//            $follows = User::where('user_id',$user_ids)->get();
+//            dd($follows);
+
+            return view('mypage.follow', compact('follows'));
+        }
+        else {
+            return redirect('/')->with('error','他のユーザーのページです。');
+        }
+    }
+
+    public function follower(User $user)
+    {
+        //ログインユーザーのみ閲覧可能
+        if (!Auth::check()) {
+            return redirect('/')->with('error', 'アクセス権限がありません');
+        }
+        if(auth()->user()->id == $user->id) {
+            //投稿状態に応じて、投稿データ取得
+            $followers = $user->profile->followers()->with('profile')->get();
+
+//            dd($follower);
+            $title = "フォロワー";
+            return view('mypage.follower', compact('followers','title'));
+        }
+        else {
+            return redirect('/')->with('error','他のユーザーのページです。');
         }
     }
 }
