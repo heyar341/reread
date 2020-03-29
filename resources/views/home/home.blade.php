58970affb266bf2f79fba92d3bb83dac2adb985d
heyar341 <kityonya@gmail.com>
2020-03-29 19:24:17 +0900

    お気に入りなど、iphone5で表示が乱れるようなので、それぞれコラムの中央に来るように修正。

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.top')
 
 @section('content')
-    <div class="container" id="">
+    <div class="container-home container" id="">
         {{--            投稿表示の領域--}}
         <div class="mx-auto mb-2 text-muted">
             <h2>{{ $title }}</h2>
         </div>
-        <div><hr></div>
+        <div>
+            <hr>
+        </div>
         <div class="row d-flex">
             @foreach($posts as $post)
                 <div class="col-xl-4 col-lg-6 rounded mb-3">
-                    <div class="card mb-3" style="max-width: 540px;min-height: 250px">
+                    <div class="card mb-3 mx-auto" style="max-width: 540px;min-height: 250px">
                         <div class="row">
                             <div class="col-12 mt-2">
                                 <div class="mx-2" style="min-height: 2.6rem"><h5>
@@ -19,14 +21,14 @@
                                 </div>
                                 <hr>
                             </div>
-                            {{--投稿表示の左側--}}
+                            {{--投稿表示の画像部分--}}
                             <div class="col-12 mt-2" style="height: 180px">
                                 <a href="/post/{{$post->id}}">
                                     <img src="{{ $post->book->bookThumbnail }}" class="mx-auto w-80 d-block"
                                          style="display: inline-block;max-height: 180px">
                                 </a>
                             </div>
-                            {{--投稿表示の右側--}}
+                            {{--投稿表示のコメント部分--}}
                             <div class="col-12 mt-2">
                                 <hr>
                                 <div class="mx-2" style="min-height: 140px">
@@ -34,22 +36,44 @@
                                     <p>{{ $post->thumbnail_comment }}</p>
                                 </div>
                             </div>
-                            {{--投稿表示のユーザー情報--}}
+                            {{--投稿の情報--}}
                             <div class="col-12">
-                                <div class="d-flex border-top border-bottom align-items-center">
-                                    <div class="col-5"><small class="rounded px-2" {{--isliked->count()だとクエリーが発行されるので、Eloquentのオブジェクト数を数えるようにした--}}
-                                                              style="background-color: #b91d19;color: white">お気に入り数：{{ count($post->is_liked) }}</small>
+                                <div class="d-flex border-top align-items-center">
+                                    <div class="col-4 d-flex">
+                                        <div class="mx-auto"><small class="rounded px-1"
+                                                                    style="background-color: #b91d19;color: white">お気に入り</small>
+                                        </div>
                                     </div>
-                                    <div class="col-3"><small class="text-muted">閲覧数：{{ $post->viewed_count }}</small>
+                                    <div class="col-4 d-flex">
+                                        <div class="mx-auto"><small class="text-muted">閲覧数</small></div>
                                     </div>
-                                    <div class="col-4"><small
-                                            class="text-muted">{{ mb_substr($post->created_at,0,10) }}</small></div>
+                                    <div class="col-4 d-flex">
+                                        <div class="mx-auto"><small
+                                                class="text-muted">投稿日時</small></div>
+                                    </div>
                                 </div>
 
+                                <div class="d-flex border-bottom align-items-center">
+                                    <div class="col-4 d-flex">
+                                        <div class="mx-auto"><small class="text-muted"
+                                                {{--isliked->count()だとクエリーが発行されるので、Eloquentのオブジェクト数を数えるようにした--}}
+                                            >{{ count($post->is_liked) }}</small></div>
+                                    </div>
+                                    <div class="col-4 d-flex">
+                                        <div class="mx-auto"><small class="text-muted">{{ $post->viewed_count }}</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-4 d-flex">
+                                        <div class="mx-auto"><small
+                                                class="text-muted">{{ mb_substr($post->created_at,0,10) }}</small></div>
+                                    </div>
+                                </div>
+
+                                {{--投稿表示のユーザー情報--}}
                                 <div class="col-12">
                                     <div class="d-flex pb-2 align-items-center">
                                         {{--ユーザー画像--}}
-                                        <div class="ml-2 mt-2 pr-3">
+                                        <div class="ml-1 mt-2 pr-3">
                                             <img src="{{ $post->user->profile->prof_image }}"
                                                  class="w-100 rounded-circle" style="max-width: 40px">
                                         </div>
